7715308067069ed86c214ad781c091d739c47a2c
chosungsu <ski06043@naver.com>
2022-01-28 13:26:00 +0900

    update

---
+++ python_version/q_11720.py
@@ -0,0 +1,6 @@
+n = input()
+nums = input()
+total = 0
+for i in nums :
+    total += int(i)
+print(total)
---
+++ python_version/q_15596.py
@@ -0,0 +1,5 @@
+def solve(num_list):
+    result = 0
+    for num in num_list:
+        result += num
+    return result
