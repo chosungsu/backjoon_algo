182966449296b76598e152374bf29adc45e3bb58
chosungsu <ski06043@naver.com>
2022-01-28 15:42:40 +0900

    update

---
+++ java_version/q_1436.java
@@ -0,0 +1,22 @@
+package java_version;
+
+import java.util.Scanner;
+ 
+public class q_1436 {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+ 
+		int num = 666;
+		int count = 1;
+        
+		while(count != N) {
+			num++;
+			if(String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+		System.out.println(num);
+	}
+}
