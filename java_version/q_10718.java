e0aa93eefaa4ed941c5af795a615afd1b5aa88de
chosungsu <ski06043@naver.com>
2022-01-30 14:42:58 +0900

    update

--- java_version/q_10718.java
+++
@@ -1,8 +0,0 @@
-package java_version;
-
-public class q_10718 {
-    public static void main(String args[]){
-        System.out.println("강한친구 대한육군");
-        System.out.println("강한친구 대한육군");
-	}
-}
--- java_version/q_2557.java
+++
@@ -1,7 +0,0 @@
-package java_version;
-
-public class q_2557 {
-    public static void main(String args[]){
-        System.out.println("Hello World!");
-	}
-}
