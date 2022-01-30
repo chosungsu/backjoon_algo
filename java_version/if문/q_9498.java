b317ba97bf51528eed4146cbbe235e3db92244d2
chosungsu <ski06043@naver.com>
2022-01-30 14:48:32 +0900

    update

---
+++ java_version/if문/q_9498.java
@@ -0,0 +1,22 @@
+package java_version.if문;
+
+import java.util.Scanner;
+//시험 성적 구하기
+public class q_9498 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if (x >= 90) {
+            System.out.println("A");
+        } else if (x >= 80 && x < 90) {
+            System.out.println("B");
+        } else if (x >= 70 && x < 80) {
+            System.out.println("C");
+        } else if (x >= 60 && x < 70) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+	}
+}
