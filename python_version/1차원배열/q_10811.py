3c5ad7ce61318b7ecd53327c4484b81986b9e1f9
chosungsu <ski06043@naver.com>
2023-09-19 19:48:16 +0900

    add codes

---
+++ python_version/1차원배열/q_10811.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+# 바구니 초기화(N값으로)
+basket = [i for i in range(1, N + 1)]
+tmp = 0
+
+# 바구니에 공을 넣는 반복문
+for _ in range(M):
+    i, j = map(int, input().split())
+    temp = basket[i-1:j]
+    temp.reverse()
+    basket[i-1:j]=temp
+
+for b in basket:
+    print(b, end=' ')
